ff25e01be5333f70d9ebd73b5df2f3b6606c165b
hvaranasi9 <harshu.varanasi9@gmail.com>
2021-01-31 12:44:36 -0600

    Updated Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,8 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static int kIntakePower = 5;
+    public static final int VictorSPXPort = 2;
+    public static final int XboxControllerPort = 0;
+}
