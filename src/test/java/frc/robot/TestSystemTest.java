5f740d3ca7a1020ac12c57e649d5222fda530a33
Matteo <mateus.sakata@gmail.com>
2021-02-05 13:50:01 -0600

    Create Unit Tests

---
+++ src/test/java/frc/robot/TestSystemTest.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Class that tests the system test
+ */
+public class TestSystemTest {
+    @Test
+    public void test() {
+        // assert statements
+        Assert.assertEquals(0, 0);
+    }
+}
